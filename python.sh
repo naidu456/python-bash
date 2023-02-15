d06643bdb77f7eafc4e86d9418815c87c44e1e33
naidu456 <118648202+naidu456@users.noreply.github.com>
2023-02-15 18:02:34 +0530

    Update python.sh

--- python.sh
+++ python.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo yum -y install git
 sudo yum -y install python3-pip
-git clone https://github.com/Akhil90639/python.git
+git clone https://github.com/naidu456/python.git
 cd python
 pip3 install -r requirements.txt
 python3 app.py
